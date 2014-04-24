abb50b4054f1054be798e65402baba178c298a95
pister <huangsongli@WangYandandeMacBook-Air.local>
2014-04-24 15:55:59 +0800

    message field

--- wint-framework/src/main/java/wint/help/biz/result/ResultSupport.java
+++ wint-framework/src/main/java/wint/help/biz/result/ResultSupport.java
@@ -4,9 +4,8 @@ import wint.core.config.Constants;
 import wint.lang.WintException;
 import wint.mvc.flow.FlowData;
 import wint.mvc.form.Field;
-import wint.mvc.form.Form;
+import wint.mvc.form.MessageHoldField;
 import wint.mvc.form.runtime.FormFactory;
-import wint.mvc.form.runtime.InputFormFactory;
 import wint.mvc.form.runtime.RunTimeForm;
 import wint.mvc.holder.WintContext;
 
@@ -59,16 +58,18 @@ public class ResultSupport implements Result {
         FlowData flowData = WintContext.getFlowData();
         String lastFormName = (String)flowData.getAttribute(Constants.Form.LAST_FORM_NAME);
         if (lastFormName == null) {
-            throw new WintException("you must call FormService.getForm() method before Result.setFieldResultCode on this thread.");
+            throw new WintException("you must call FormService.getForm() method before Result.seResultCode(String, ResultCode) on this thread.");
         }
         FormFactory formFactory = (FormFactory)flowData.getInnerContext().get(Constants.Form.TEMPLATE_FORM_FACTORY_NAME);
 
         RunTimeForm runTimeForm = formFactory.getForm(lastFormName);
         Field field = runTimeForm.get(fieldName);
         if (field == null) {
-            throw new WintException("field not exist for name: " + fieldName);
+           field = new MessageHoldField(fieldName, resultCode.getMessage());
+           runTimeForm.getFields().put(fieldName, field);
+        } else {
+            field.setMessage(resultCode.getMessage());
         }
-        field.setMessage(resultCode.getMessage());
         hasFieldResultCodes = true;
     }
 
---
+++ wint-framework/src/main/java/wint/mvc/form/MessageHoldField.java
@@ -0,0 +1,56 @@
+package wint.mvc.form;
+
+import wint.mvc.form.config.FieldConfig;
+
+/**
+ * User: huangsongli
+ * Date: 14-4-24
+ * Time: 下午3:46
+ */
+public class MessageHoldField implements Field {
+
+    private String name;
+
+    private String message;
+
+    public MessageHoldField(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLabel() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void setValue(String value) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getValue() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String[] getValues() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void setValues(String[] values) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public FieldConfig getFieldConfig() {
+        throw new UnsupportedOperationException();
+    }
+}
