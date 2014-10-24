8140a64c270bd8d26729e66e6784863f4f6e1fc4
HuangSongli <huangsongli@HuangSonglideMacBook-Pro.local>
2014-10-24 17:42:52 +0800

    fixed

--- wint-framework/src/main/java/wint/sessionx/provider/sessionid/DefaultSessionIdGenerator.java
+++ wint-framework/src/main/java/wint/sessionx/provider/sessionid/DefaultSessionIdGenerator.java
@@ -1,6 +1,8 @@
 package wint.sessionx.provider.sessionid;
 
-import java.security.SecureRandom;
+import wint.help.codec.MD5;
+
+import java.util.UUID;
 
 /**
  * User: longyi
@@ -9,31 +11,10 @@ import java.security.SecureRandom;
  */
 public class DefaultSessionIdGenerator implements SessionIdGenerator {
 
-    private final SecureRandom numberGenerator = new SecureRandom();
-
-    private static final int LENGTH = 32;
-
-    public DefaultSessionIdGenerator() {
-        byte[] seed = numberGenerator.generateSeed(32);
-        numberGenerator.setSeed(seed);
-    }
-
-    static private String toHexString(byte[] ba) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (byte b : ba) {
-            String s = Integer.toHexString(b & 0xff);
-            if (s.length() == 1) {
-                stringBuilder.append('0');
-            }
-            stringBuilder.append(s);
-        }
-        return stringBuilder.toString();
-    }
-
     public String generateSessionId() {
-        byte[] total = new byte[LENGTH];
-        numberGenerator.nextBytes(total);
-        return toHexString(total);
+        String s1 = UUID.randomUUID().toString();
+        String s2 = UUID.randomUUID().toString();
+        return MD5.encrypt(s1) + MD5.encrypt(s2);
     }
 
 
